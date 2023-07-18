b3dd6cc5ee5889ec6f6073368d3a25f99a65d5f0
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-07-18 05:16:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
+
 function App() {
   return (
     <div className="App">
